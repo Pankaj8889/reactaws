5ed57325f0de422c8519472ed94475cb3faf9b40
Pankaj8889 <pankajsaini8889@gmail.com>
2022-03-01 21:02:00 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <NavBar width={"100vw"}/>
       <HeroLayout2 width={"100vw"}/>
       <Features2x3 width={"100vw"}/>
-      <MarketingFooter width={"100vw"}/>
+      <MarketingFooter width={"100vw"}/> 
     </div>
   );
 }
